9e23aacda438f890546a180dcd58e0e7fd538298
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2024-09-30 11:32:42 +0000

    esercizio

---
+++ informatica/A<>=B.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main()
+{ 
+	int a,b;	
+	
+	a=0;
+	b=0;
+	
+	
+	printf("inserisci il primo valore:");
+	scanf("%d",&a);
+
+	printf("inserisci il secondo valore:");
+	scanf("%d",&b);
+	
+
+
+	if(a == b)
+	{ 
+		printf("il primo valore %d è uguale del secondo %d", a,b);  
+	}
+	else 
+	{
+		printf("il primo valore %d non è uguale del secondo %d", a,b);
+
+            if(a < b)
+	{ 
+		printf("il primo valore %d è minore del secondo %d", a,b);  
+	}
+	else 
+	{
+		printf("il primo valore %d non è minore del secondo %d", a,b);
+
+            if(a > b)
+	{ 
+		printf("il primo valore %d è maggiore del secondo %d", a,b);  
+	}
+	else 
+	{
+		printf("il primo valore %d non è maggiore del secondo %d", a,b);
+	}
+	}
+
+	}
+
+   
+}
