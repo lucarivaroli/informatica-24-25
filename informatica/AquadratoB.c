68cc7d5416565e9dadec4bad3405a58cf66d2b45
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2024-09-30 11:09:47 +0000

    esercizio

--- informatica/AquadratoB.c
+++ informatica/AquadratoB.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 int main()
 { 
-	int a,doppio;	//dichiarazione delle variabili
+	float a,b,quadrato;	
 	
 	a=0;
-	doppio=0;
+	b=0;
+	quadrato=0;
 	
+	printf("inserisci il primo valore:");
+	scanf("%f",&a);
+
+	printf("inserisci il secondo valore:");
+	scanf("%f",&b);
 	
-	printf("inserisci un numero: ");
-	scanf("%d",&a);
-	doppio= a*2;
+	quadrato=b*b;
 	
-	printf("il doppio di a e: %d", doppio);
-	
-	return 0;
+	printf("il doppio è: %f",quadrato);
+
+	if(a == quadrato)
+	{ 
+		printf("il primo valore %f è il quadrato del secondo %f", a,b);  
+	}
+	else 
+	{
+		printf("il primo valore %f non è il quadrato del secondo %f", a,b)
+	}
+
+		return 0;
 	
 }
