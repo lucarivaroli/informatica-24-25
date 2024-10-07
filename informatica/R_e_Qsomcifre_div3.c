1016bc6e50ed4bef3880c30fdbace682c12fd8f6
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2024-10-07 11:54:02 +0000

    esscuola

---
+++ informatica/R_e_Qsomcifre_div3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+   
+    int n1=0, ope1=0, ope2=0, ope3=0;
+
+    printf("Inserisci il primo numero: ");
+    scanf("%d", &n1);
+    ope=n1%10;
+    ope1=n1%10;
+    ope2=n1%100
+
+    /*if(cifra%2==0)
+        {
+        printf("il numero è pari: ",n1); 
+        }
+     else
+        {
+        printf("il numero è dispari: ",n1); 
+        }*/
+    
+
+return 0;
+    
+    
+}
