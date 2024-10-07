192506795be9ef2321eb5abec7f1f3b8a963610f
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2024-10-07 10:37:31 +0000

    esscuola

---
+++ informatica/media_min_max.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() 
+{
+    float n1=0;
+    float n2=0;
+    float n3=0;
+    float media=0;
+    float max=0;
+    float min=0;
+    
+    printf("Inserisci i tre valori: ");
+    scanf("%f%f%f", &n1, &n2, &n3);
+
+    max=n1
+    min=n1
+    (if max<n2)
+        max=n2;
+    else
+        if(max<n3)
+            max=n3;
+
+   
+   
+    
+    
+
+    return 0;
+}
