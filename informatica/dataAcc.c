ea09eab0f5c482dd1d54715044ebd04457c64d95
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2024-10-08 11:54:25 +0000

    es

--- informatica/dataAcc.c
+++ informatica/dataAcc.c
@@ -5,7 +5,7 @@ verificare se la data è accettabile*/
 
 int main() 
 {
-    int giorno=0,mese=0,anno=0;
+    int giorno=0,mese=0,anno=0, bisestile=0;
 
   
     printf("Inserisci giorni mesi e anni: ");
@@ -17,12 +17,36 @@ int main()
     {
         if(anno%400==0){
             printf("l'anno è bisestile");
+            bisestile=1;
         }
     } 
     else
     {
         if(anno%4==0){
-            printf("l'anno è bisestile")
+            printf("l'anno è bisestile");
+            bisestile=1;
+        }
+    }
+
+// controllo del mese edel giorno
+    if(mese>=1 && mese<=12){
+        if(mese==2){
+            if(giorno>=1 && giorno<=28+bisestile){
+                printf("la data è accettabile");
+            }
+        }
+        else{
+            if(mese==11 || mese==4 || mese==6 || mese==9){
+                if(giorno>=1 && giorno<=30){
+                    printf("la data è accettabile");
+
+                }
+            }
+            else{
+                if(giorno>=1 && giorno<=31){
+                    printf("la data è accettabile");  
+                }
+            }
         }
     }
 
