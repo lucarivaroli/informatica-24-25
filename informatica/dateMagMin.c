b818b3a5b4d305afca862be410eef0b4fe628585
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2024-10-13 18:24:48 +0000

    es casa

--- informatica/dateMagMin.c
+++ informatica/dateMagMin.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 int main() {
+
     int g1, m1, a1;
     int g2, m2, a2;
 
-    // Input delle due date
+ 
     printf("Inserisci la prima data (gg mm aaaa): ");
     scanf("%d %d %d", &g1, &m1, &a1);
 
     printf("Inserisci la seconda data (gg mm aaaa): ");
     scanf("%d %d %d", &g2, &m2, &a2);
 
-    // Confronto delle date
+ 
     if (a1 > a2) {
         printf("La data più recente è: %02d/%02d/%04d\n", g1, m1, a1);
     } else if (a1 < a2) {
