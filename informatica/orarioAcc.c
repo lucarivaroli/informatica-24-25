8363e15d49a239a4dccff356c61cc719af6806b5
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2024-10-14 11:04:40 +0000

    esscuola

--- informatica/orarioAcc.c
+++ informatica/orarioAcc.c
@@ -13,26 +13,21 @@ int main() {
     scanf("%d %d %d", &o2, &m2, &s2);
 
  
-    if(s1<60 && m1<60 && o1<24 && s2<60 && m2<60 && o2<24){
-        printf("l'orario e valida: ")
-    }else
+    if(s1<60 && m1<60 && o1<24 && s2<60 && m2<60 && o2<24)
     {
-        printf("l'orario non e valido: ")
-    }
-    
-    
-    if (o1 > o2) {
-        printf("La ora più recente è: %02d %02d %02d\n", o1, m1, s1);
+        printf("l'orario e valido: ");
+     if (o1 > o2) {
+        printf("La ora che viene prima è: %02d %02d %02d\n", o1, m1, s1);
     } else if (o1 < o2) {
-        printf("La ora più recente è: %02d %02d %02d\n", o2, m2, s2);
+        printf("La ora che viene prima è: %02d %02d %02d\n", o2, m2, s2);
     } else {
         if (m1 > m2) {
-            printf("La ora più recente è: %02d %02d %02d\n", o1, m1, s1);
+            printf("La ora che viene prima è: %02d %02d %02d\n", o1, m1, s1);
         } else if (m1 < m2) {
-            printf("La ora più recente è: %02d %02d %02d\n", o2, m2, s2);
+            printf("La ora che viene prima è: %02d %02d %02d\n", o2, m2, s2);
         } else {
             if (s1 > s2) {
-                printf("La ora più recente è: %02d %02d %02d\n", o1, m1, s1);
+                printf("La ora che viene prima è: %02d %02d %02d\n", o1, m1, s1);
             } else if (s1 < s2) {
                 printf("La ora più recente è: %02d %02d %02d\n", o2, m2, s2);
             } else {
@@ -41,5 +36,12 @@ int main() {
         }
     }
 
+    }else
+    {
+        printf("l'orario non e valido: ");
+    }
+    
+    
+   
     return 0;
 }
