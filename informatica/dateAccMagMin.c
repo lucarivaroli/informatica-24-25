5dc3b3f92594af646749ce1aa4db4d1dc25e5aba
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2024-10-15 11:48:44 +0000

    esrisolto

--- informatica/dateAccMagMin.c
+++ informatica/dateAccMagMin.c
@@ -5,7 +5,7 @@ int main() {
     int g1, m1, a1;
     int g2, m2, a2;
     int bisestile;
-    int anno, mese, giorno;
+    
 
  
     printf("Inserisci la prima data gg mm aaaa: ");
@@ -14,22 +14,22 @@ int main() {
     printf("Inserisci la seconda data gg mm aaaa: ");
     scanf("%d %d %d", &g2, &m2, &a2);
 
-        if(anno%100==0){
-        if (anno%400==0){
-            printf("L'anno è bisestile");
+    if(a1%100==0){
+    if (a1%400==0){
+            printf("L'a1 è bisestile");
             bisestile=1;
         }
     }
     else{
-        if (anno%4==0){
-            printf("L'anno è bisestile");
+        if (a1%4==0){
+            printf("L'a1 è bisestile");
             bisestile=1;
         }
     }
-    //controllo del mese e del giorno
-    if (mese>=1 && mese <=12){
-        if (mese==2){
-            if(giorno>=1 && giorno<=28+bisestile){
+    //controllo del m1 e del g1
+    if (m1>=1 && m1 <=12){
+        if (m1==2){
+            if(g1>=1 && g1<=28+bisestile){
                 printf("\nLa data è accettabile");
             }
             else{
@@ -37,8 +37,8 @@ int main() {
             }
         }
         else{
-            if( mese==11 || mese==4 || mese==6 || mese==9){
-                if (giorno>=1 && giorno<=30){
+            if( m1==11 || m1==4 || m1==6 || m1==9){
+                if (g1>=1 && g1<=30){
                     printf("\nLa data è accettabile");
                 }
                 else{
@@ -46,7 +46,55 @@ int main() {
                 }
             }
             else{
-                if (giorno>=1 && giorno<=31){
+                if (g1>=1 && g1<=31){
+                    printf("\nLa data è accettabile");
+                }
+                else{
+                    printf("\nLa data non è accettabile");
+                }
+            }
+        }
+    }
+    else{
+        printf("\nLa data non è accettabile");
+    }
+
+
+    //controllo seconda data
+
+        if(a2%100==0){
+    if (a2%400==0){
+            printf("L'a1 è bisestile");
+            bisestile=1;
+        }
+    }
+    else{
+        if (a2%4==0){
+            printf("L'a2 è bisestile");
+            bisestile=1;
+        }
+    }
+    //controllo del m2 e del g2
+    if (m2>=1 && m2 <=12){
+        if (m2==2){
+            if(g2>=1 && g2<=28+bisestile){
+                printf("\nLa data è accettabile");
+            }
+            else{
+                printf("\nLa data non è accettabile");
+            }
+        }
+        else{
+            if( m2==11 || m2==4 || m2==6 || m2==9){
+                if (g2>=1 && g2<=30){
+                    printf("\nLa data è accettabile");
+                }
+                else{
+                    printf("\nLa data non è accettabile");
+                }
+            }
+            else{
+                if (g2>=1 && g2<=31){
                     printf("\nLa data è accettabile");
                 }
                 else{
