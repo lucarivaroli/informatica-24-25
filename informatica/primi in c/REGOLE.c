effeeaff2a4a175b7fc6ea61fe19cc091139edee
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2024-11-19 15:08:53 +0000

    regole

---
+++ informatica/primi in c/REGOLE.c
@@ -0,0 +1,41 @@
+/*generato un valore, dire chi dei due si avvicina di piu */
+
+k=rand()%9+0; //genera il numero casuale
+printf("il valore di k e: \n", k);
+
+scarto1=n1-k;
+scarto2=n2-k;
+//se uno dei due scarti e negativo ce da fare *(-1)
+if(scarto1<scarto2)
+    printf("il numero che si avvicina di piu è %d", n1);
+else
+    printf("il numero che si avvicina di piu è %d", n2);
+break;
+
+/*contare e leggere le cifre di un numero*/
+    q=numero
+    if(q!=0)
+        r=q%10;
+        q=q/10;
+        cntc++;
+        printf("la cifra delle unita vale %d", r);
+        printf("il numero e composto da %d cifre", cnt);
+/*comunicare il numero di cifre e comunicare qaunte volte compare la cifra(k)*/
+    q=numero
+    if(q!=0)
+        r=q%10;
+        q=q/10;
+        cntc++;
+        printf("la cifra delle unita vale %d", r);
+        if(r==k)//se il resto (quindi la cifra ) e uguale il valore di k(inserito dall'utente), cntk lo conta 
+            cntk++;
+/*sommare un numero ad un'altro e fare la media*/
+    printf
+    scanf
+    somma=numero
+    cnt++
+
+    if
+    somma=somma+numero;
+    cnt++;
+    media= somma/cnt
