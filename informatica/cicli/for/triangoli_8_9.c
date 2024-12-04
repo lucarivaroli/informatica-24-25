9c1b2519dae49ab46ff5395ab96e75496fc8ce29
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2024-12-04 08:42:01 +0000

    es

--- informatica/cicli/for/triangoli_8_9.c
+++ informatica/cicli/for/triangoli_8_9.c
@@ -14,5 +14,14 @@ int main(){
         }
         printf("\n");
     }
-    return 0;
+
+
+    for (int i =1;i<=9;i++ ){
+        for(int j=1;j<=9; j++){
+            
+        }
+
+            printf("\n");
+    }
+}
 }
