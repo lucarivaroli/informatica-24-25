34402cc53ea9ac14bb7e031e5297b14f8a26998b
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2025-02-07 06:51:38 +0000

    es

--- INFORMATICA/funzioni/raggio_circonf.c
+++ INFORMATICA/funzioni/raggio_circonf.c
@@ -2,18 +2,24 @@
 #include <stdio.h>
 const float pigreco=3.14;
 
-void circonf(float *_raggio, float *_diametro);
+void circonf(float *_raggio, float *_diametro, float *_area );
 
 int main(){
     float raggio=0;
     float diametro=0;
+    float area=0;
 
     printf("inserisci il raggio: ");
     scanf("%d", &raggio);
 
-    circonf(&raggio, &diametro);
+    circonf(&raggio, &diametro, &area);
 }
 
-void circonf(float *_raggio, float *_diametro){
+void circonf(float *_raggio, float *_diametro, float *_area){
+    *_diametro=2*(*_raggio);
+    *_area=pigreco*(*_raggio)*(*_raggio);
+    
+
+    
     
 }
