59f26f80cebf320c4a673cfa0326e5ba127f4fd2
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2025-03-20 10:00:41 +0000

    correzione

--- INFORMATICA/vettori/libreria.c
+++ INFORMATICA/vettori/libreria.c
@@ -2,12 +2,13 @@
 #include "libreria.h"   //prototipi delle funzioni
 
 void richiediValori(int vettore[], int dim){
-    int n=0;
-    for(int i=0; i<dim; i++){
-        printf("Inserisci il valore in posizione %d: ", i);
+    int n=0, i=0;
+    while(i<dim){
+        printf("Inserisci il %do valore: ", i+1);
         scanf("%d", &n);
         if(n%2==0){
             vettore[i]=n;
+            i++;
         }
     }
 }
