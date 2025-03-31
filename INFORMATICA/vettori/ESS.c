b32826bdf9ddbb2e2aa451fb1a434deb9c2c5517
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2025-03-31 09:55:10 +0000

    scuola

---
+++ INFORMATICA/vettori/ESS.c
@@ -0,0 +1,24 @@
+/*dopo avere caricato un vettore numerico confrontare 
+la somma delle componenti di indice pari con la somma
+delle componenti dispari e dire se la somma maggiore 
+e divisibile per un valore assegnato k (diverso da 0)*/
+
+#include<stdio.h>
+#include "libreria.h"
+#include "libreria.c"
+#define dim 4
+
+int main(){
+    int vett1[dim];
+    int vett2[dim];
+
+    riempiVettore(vett1, dim);
+    riempiVettore(vett2, dim);
+
+    do
+    {
+        printf("inserisci il valore di k");
+        scan
+    } while (k<0);
+    
+}
