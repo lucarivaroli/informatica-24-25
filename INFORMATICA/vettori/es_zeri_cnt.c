50608d9bd0985b7363f5ed5f1cbc42fd63fb39ec
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2025-04-11 18:38:20 +0000

    progetto

---
+++ INFORMATICA/vettori/es_zeri_cnt.c
@@ -0,0 +1,19 @@
+/*Caricare
+da tastiera un vettore di N elementi. Scorrendo il vettore, al primo
+zero incontrato stampare la somma di tutti gli elementi considerati e
+il loro numero*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "libreria.h"
+#include "libreria.c"
+
+#define dim 15
+
+
+int main(){
+
+    int vett[dim];
+    riempiVettore(vett, dim);
+
+}
