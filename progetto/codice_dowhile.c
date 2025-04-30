579e6ed0cce8564f3f6c7f5f1dd3ddbc7a6a0956
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2025-04-30 15:16:14 +0000

    progetto

--- progetto/codice_dowhile.c
+++ progetto/codice_dowhile.c
@@ -1,5 +1,7 @@
+//FUNZIONANTE
+
 #include <Wire.h>                   // Comunicazione con display LCD I2C
-#include <LiquidCrystal_I2C.h>      // Libreria per il display LCD I2C
+#include <LiquidCrystal_I2C.h>      //  display LCD I2C
 
 #define TRIG 7                      // Pin di uscita sensore ultrasuoni
 #define ECHO 6                      // Pin di entrata sensore ultrasuoni
@@ -8,7 +10,7 @@
 #define LED_LAMPEGGIO 4             // LED verde che lampeggia
 #define LUMINOSITA_PIN A0           // Pin del fotoresistore (luminosità)
 
-LiquidCrystal_I2C lcd(0x27, 20, 4); // Inizializzazione display 20x4 I2C
+LiquidCrystal_I2C lcd(0x27, 20, 4); // Inizializzazione display 
 
 // Funzione per far lampeggiare un LED
 void lampeggiaLED(int pin, int lampeggi, int durata) {
@@ -18,6 +20,19 @@ void lampeggiaLED(int pin, int lampeggi, int durata) {
         digitalWrite(pin, LOW);
         delay(durata);
     }
+}
+
+//funzione che misura la distanza usando il sensore a ultrasuoni (HC-SR04)
+long misuraDistanza() {
+    digitalWrite(TRIG, LOW);            //stabilizza il segnale
+    delayMicroseconds(2);
+    digitalWrite(TRIG, HIGH);           //inizio della misurazione
+    delayMicroseconds(10);
+    digitalWrite(TRIG, LOW);            //fine misurazione
+
+    long durata = pulseIn(ECHO, HIGH);        // Tempo che l'impulso impiega a tornare al sensore
+    long distanza = durata * 0.034 / 2;       
+    return distanza;
 }
 
 // Inizializzazione, eseguita una sola volta
@@ -38,18 +53,6 @@ void setup() {
     delay(1000);
 }
 
-// Misura la distanza usando il sensore a ultrasuoni (HC-SR04)
-long misuraDistanza() {
-    digitalWrite(TRIG, LOW);            //stabilizza il segnale
-    delayMicroseconds(2);
-    digitalWrite(TRIG, HIGH);           //inizio della misurazione
-    delayMicroseconds(10);
-    digitalWrite(TRIG, LOW);            //fine misurazione
-
-    long durata = pulseIn(ECHO, HIGH);        // Tempo che l'impulso impiega a tornare al sensore
-    long distanza = durata * 0.034 / 2;       
-    return distanza;
-}
 
 // Ciclo principale, eseguito continuamente
 void loop() {
@@ -74,14 +77,18 @@ void loop() {
 
         // DO-WHILE: lampeggia finché il bicchiere è sotto (<=10 cm)
         do {
+            
             lampeggiaLED(LED_LAMPEGGIO, 1, 200);
-            distanza = misuraDistanza(); // aggiorna la distanza 
+            distanza = misuraDistanza(); // aggiorna la distanza
+            
+            digitalWrite(RELAY, LOW); // Accende il relay
+            lcd.print("Pompa: ACCESA");
+            lcd.setCursor(0, 2);
+            lcd.print("Grazie, arrivederci! ");
+
         } while (distanza <= 10);
 
-        digitalWrite(RELAY, LOW); // Accende il relay
-        lcd.print("Pompa: ACCESA");
-        lcd.setCursor(0, 2);
-        lcd.print("Grazie, arrivederci! ");
+        
 
     } else {
         
