c2a06ae616fc33f94cf3a478eb7e40d8a901fedc
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2025-09-16 07:43:33 +0000

    3

---
+++ INFORMATICA/3I/cicli/vettori/libreria.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "libreria.h"   //prototipi delle funzioni
+
+void riempiVettore(int vettore[], int dim){
+    for(int i=0; i<dim; i++){
+        printf("inserici l'elemento in posizione di %d: ", i);
+        scanf("%d", &vettore[i]);
+    }
+}
+
+
+void stampaVettore(int vettore[], int dim, char sep){
+    for(int i=0; i<dim; i++){
+        printf("%d %c", vettore[i], sep);
+    }
+}
+
+void caricaVettoreRandom(int vettore[], int dim, int min, int max){
+    srand(time(NULL));
+    for(int i=0; i<dim; i++){
+        vettore[i]=rand()%(max-min+1)+min;
+    }
+}
+
+int sommaVettore(int vettore[], int dim){
+    int somma=0;
+    for(int i=0; i<dim; i++){
+        somma+=vettore[i];
+    }
+    return somma;
+}
+
+float mediaVettore(int vettore[], int dim){
+    return (float)sommaVettore(vettore, dim)/dim;
+}
+
+int contaValore(int vettore[], int dim, int valore){
+    int contatore=0;
+    for(int i=0; i<dim; i++){
+        if(vettore[i]==valore){
+            contatore++;
+        }
+    }
+    return contatore;
+}
+
+int ricercaSequenziale(int *vettore, int dim, int valore){
+    for(int i = 0; i<dim; i++){
+        if(vettore[i]==valore){
+            return i;
+        }
+    }
+    return -1;
+
+}
+
+int ricercaBinaria(int vettore[], int dim, int estInf, int estSup, int valore){
+    int media = (estSup+estInf)/2;
+    if(estInf > estSup)
+        return -1;
+    if(vettore[media]== valore)
+        return media;
+    if(vettore[media] > valore)
+        return ricercaBinaria( vettore,dim, estInf, media-1, valore);
+    else 
+        return ricercaBinaria( vettore,dim, media+1, estSup, valore);
+}
+
+int trovaPosizione (int vett[], int dim, int num){
+    int i=0;
+    while(vett[i]<num && i<dim){
+        i++;
+    }
+    return i;
+}
+
+
+void shiftDx (int vett[], int dim, int pos){
+    for(int i=dim; i>pos; i--){
+        vett[i]=vett[i-1];
+    }
+}
+
+
+void riempiVettoreOrdinatoCasuale(int vett[], int dim, int minimo, int massimo) {
+    srand(time(NULL));
+    int num, pos;
+    vett[0] = rand()%(massimo-minimo+1)+minimo;
+    for(int i=1; i<dim; i++){
+        num = rand()%(massimo-minimo+1)+minimo;
+        pos = trovaPosizione(vett, i, num); 
+        shiftDx(vett, i, pos);
+        vett[pos] = num;
+    }
+}
+
+
+void scambiavettori(int vett[], int dim, int vett2[], int dim2){
+    int temp;
+    if(dim==dim2){
+        for(int i=0; i<dim; i++){
+            temp = vett[i];
+            vett[i] = vett2[i];
+            vett2[i] = temp;
+        }
+    }
+}
+
+
+void caricaBubble(int vett[], int dim){
+    for(int i=0; i<dim-1; i++){
+        for(int j=0; j<dim-i-1; j++){
+            if(vett[j]>vett[j+1]){
+                int temp = vett[j];
+                vett[j] = vett[j+1];
+                vett[j+1] = temp;
+            }
+        }
+    }
+}
+
+
+
+void scambiominmax(int vett[], int DIM){
+    int min, max, temp, i_min, i_max;
+    min=vett[0];
+    max=vett[0];
+
+    for(int i=1; i<DIM; i++){
+        if(vett[i]<min){
+            min=vett[i];
+            i_min=i;
+        }
+
+        if (vett[i]>max)
+        {
+            max=vett[i];
+            i_max=i;
+        }
+        
+    }
+
+    vett[i_min]=max;
+    vett[i_max]=min;
+
+}
---
+++ INFORMATICA/3I/cicli/vettori/libreria.h
@@ -0,0 +1,125 @@
+/*
+    PROTOTIPI DELLE FUNZIONI
+*/
+
+/**
+ * funzione che stampa gli elementi di un vettore.
+ * @param int [] vettore di interi 
+ * @param int dimensione del vettore 
+ * @param char separatore tra un elemento e un altro 
+ */
+void stampaVettore(int vettore[], int dim, char sep);
+
+/**
+ * funzione che riempie un vettore con valori scelti dall'utente.
+ * @param int [] vettore di interi 
+ * @param int dimensione del vettore 
+ */
+void riempiVettore(int vettore[], int dim);
+
+void richiediValori(int vettore[], int dim);
+/**funzione che richiede i valori da inserire in un vettore
+ * @param int vettore[] vettore in cui inserire i valori
+ * @param int dim dimensione del vettore
+ */
+
+void caricaVettoreRandom(int vettore[], int dim, int min, int max);
+/**funzione che carica un vettore con valori random
+ * @param int vettore[] vettore da caricare
+ * @param int dim dimensione del vettore
+ * @param int min valore minimo
+ * @param int max valore massimo
+ */
+
+float mediaVettore(int vettore[], int dim);
+/**funzione che calcola la media dei valori di un vettore
+ * @param int vettore[] vettore di cui calcolare la media
+ * @param int dim dimensione del vettore
+ * @return float media dei valori del vettore
+ */
+
+int sommaVettore(int vettore[], int dim);
+/**funzione che calcola la somma dei valori di un vettore
+ * @param int vettore[] vettore di cui calcolare la somma
+ * @param int dim dimensione del vettore
+ * @return int somma dei valori del vettore
+ */
+
+int contaValore(int vettore[], int dim, int valore);
+/**funzione che conta quante volte un valore compare in un vettore
+ * @param int vettore[] vettore in cui cercare il valore
+ * @param int dim dimensione del vettore
+ * @param int valore valore da cercare
+ * @return int numero di volte in cui il valore compare nel vettore
+ */
+
+/**
+ * questa funzione restituisce la posizione del primo elemento nell'array che ha valore uguale alla variabile valore, se non trova l'elemento restituisce -1
+ * @param int* vettore in ingresso
+ * @param int dimensione del vettore
+ * @param int valore da ricercare
+ * @return int posizione del primo elemento uguale a valore o -1 se non trovato
+ */
+int ricercaSequenziale(int *vettore, int dim, int valore);
+
+
+/**
+ * questa funzione restituisce la posizione del primo elemento in un array ORDINATO in ordine crescente che ha valore uguale alla variabile valore, se non trova l'elemento restituisce -1
+ * @param int[]vettore in ingresso
+ * @param int dimensione del vettore
+ * @param int indice dell'elemento da cui parte la ricerca
+ * @param int indice dell'elemento a cui arriva la ricerca
+ * @param int valore da ricercare
+ * @return int posizione del primo elemento uguale a valore o -1 se non trovato
+ */
+int ricercaBinaria(int array[], int dim, int estInf, int estSup, int valore);
+
+/**
+ * Funzione che riempie un vettore di dimensione dim con numeri casuali in ordine crescente
+ * @param int[] vett vettore da riempire
+ * @param int dim dimensione del vettore
+ * @param int minimo valore minimo
+ * @param int massimo valore massimo
+ */
+void riempiVettoreOrdinatoCasuale(int vett[], int dim, int minimo, int massimo);
+
+int trovaPosizione (int vett[], int dim, int num);
+/**
+ * Funzione che restituisce la posizione in cui inserire un numero in un vettore ordinato in ordine crescente
+ * @param int[] vett vettore in cui cercare la posizione
+ * @param int dim dimensione del vettore
+ * @param int num numero da inserire
+ */
+
+ void shiftDx (int vett[], int dim, int pos);
+/**
+ * Funzione che sposta tutti gli elementi di un vettore a destra a partire dalla posizione pos
+ * @param int[] vett vettore da spostare
+ * @param int dim dimensione del vettore
+ * @param int pos posizione da cui iniziare lo spostamento
+ */
+
+ void scambiavettori(int vett[], int dim, int vett2[], int dim2);
+/**
+ * Funzione che scambia i valori di due vettori
+ * @param int[] vett vettore da scambiare
+ * @param int dim dimensione del vettore
+ * @param int[] vett2 vettore da scambiare
+ * @param int dim2 dimensione del vettore
+ */
+
+void caricaBubble(int vett[], int dim);
+/**
+ * Funzione che ordina un vettore di interi in ordine crescente utilizzando l'algoritmo Bubble Sort
+ * @param int[] vett vettore da ordinare
+ * @param int dim dimensione del vettore
+ */
+
+
+
+void scambiomaxmin(int vett[], int dim);
+/**
+ * Funzione che scambia il valore massimo e il valore minimo di un vettore
+ * @param int[] vett vettore da modificare
+ * @param int dim dimensione del vettore
+ */
