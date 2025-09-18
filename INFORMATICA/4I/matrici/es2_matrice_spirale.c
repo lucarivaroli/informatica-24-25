1a7586bade6f9b01cf5b8108d1a73540696ca790
lucarivaroli <luca.rivaroli@galileiostiglia.edu.it>
2025-09-18 07:53:19 +0000

    scuola

--- INFORMATICA/4I/matrici/es2_matrice_spirale.c
+++ INFORMATICA/4I/matrici/es2_matrice_spirale.c
@@ -2,36 +2,36 @@
 #include <stdio.h>
 #define N 4
 
-void riempimat(int mat[][N]){
-    int num=0, i;
+void riempimat(int _mat[][N]){
+    int num=1, i;
     int top=0, botton=N-1, left=0, right=N-1;
 
     while(num<=N*N){
         //sinistra verso destra
         for(i=left; i<=right;i++)
-            mat[top][i]=num+1;
+            _mat[top][i]=num++;
         top++;
         //alto verso il basso
         for(i=top; i<=botton; i++)
-            mat[i][right]=num+1;
+            _mat[i][right]=num++;
         right--;
         //destra verso sinistra
-        for(i=right; i<=left; i--)
-            mat[botton][i]=num+1;
+        for(i=right; i>=left; i--)
+            _mat[botton][i]=num++;
         botton--;
         //basso verso l'alto
-        for(i=botton;i<=top; i--)
-            mat[i][left]=num+1;
+        for(i=botton;i>=top; i--)
+            _mat[i][left]=num++;
         left++;
     }
 
 
 }
 
-void stampamat(int mat[][N]){
+void stampamat(int _mat[][N]){
     for(int i=0; i<N; i++){
         for(int j=0; j<N; j++){
-            printf("%d\t ", mat[i][j]);
+            printf("%d\t ", _mat[i][j]);
         }
         printf("\n");
     }
@@ -42,7 +42,7 @@ int main() {
     
     int mat[N][N];
     riempimat(mat);
-    printf("la matrice a spirale e: ");
+    printf("la matrice a spirale e:\n ");
     stampamat(mat);
 
     
